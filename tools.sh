c65d1fa395d819515eca38e251232ca0e38ef436
ayush khatkar <91492885+ayushkhatkar@users.noreply.github.com>
2022-06-25 16:28:39 +0530

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+apt update && apt upgrade;
+wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz;
+sudo tar -xvf go1.18.3.linux-amd64.tar.gz;
+sudo mv go /usr/local;
+export GOROOT=/usr/local/go;
+export GOPATH=$HOME/go;
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH;
+echo "
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+" >> /root/.bashrc;
+sudo snap install amass;
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest;
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest;
+go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest;
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest;
+apt install nmap;
+apt install whois;
+go install github.com/ffuf/ffuf@latest;
+go install github.com/lc/gau/v2/cmd/gau@latest;
+go install github.com/tomnomnom/waybackurls@latest;
+go install github.com/tomnomnom/meg@latest;
+go install github.com/tomnomnom/qsreplace@latest;
+go install github.com/tomnomnom/gf@latest && cp ~/go/bin/gf /usr/bin/ && gf -h && mkdir ~/.gf && git clone https://github.com/Sherlock297/gf_patterns.git && cd gf_patterns/ && cp *.json ~/.gf && gf -list;
+echo " Don't Forget to run ' source .bashrc ' after completion"
